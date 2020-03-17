bdcac7e124530f2ae59eae94fa5ef26784f9b3bd
Andrew Haberman <a.haberman25@gmail.com>
2020-03-17 11:25:55 -0400

    added styled components and styled

--- team-builder/src/Components/Users.Component.jsx
+++ team-builder/src/Components/Users.Component.jsx
@@ -1,19 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Users = props => {
-    
+const Users = props => {    
     // console.log(props.regUsers)
+    const Container = styled.div` 
+        padding: 10px;
+        width: 50%;
+        margin: 15px auto;
+    `;
+    const RegList = styled.div` 
+        padding: 10px;
+        width: 75%;
+        margin: 15px auto;
+        text-align: left;
+        border: black 1px solid;
+        background-color: rgba(149,165,166,.6 );
+    `;
+
     return(
-        <div className='regUsers-container'>            
+        <Container className='regUsers-container'>            
             {// map through the data being sent
                 props.regUsers.map(user => (
-                <div className="regUserList" key={user.id}>
-                    <p>{user.username}</p>
-                    <p>{user.email}</p>
-                    <p>{user.role}</p>
-                </div>
+                <RegList className="regUserList" key={user.id}>
+                    <p><b>User</b></p>
+                    <p><b>Username:</b> {user.username}</p>
+                    <p><b>Email:</b> {user.email}</p>
+                    <p><b>Role:</b> {user.role}</p>
+                </RegList>
             ))}
-        </div>
+        </Container>
     )
 }
 
